e1b7957c612a1d96b1d9a785f208bd38c2490df5
Stetson Ramey <stetson.ramey@gmail.com>
2019-09-10 11:49:30 -0700

    built out the offer page

---
+++ tailwind.config.js
@@ -0,0 +1,10 @@
+module.exports = {
+  theme: {
+    colors: {
+      logoTeal: '#6ac3ba',
+      logoPurple: '#f067a0',
+      secondary: '#999',
+      footerBackground: '#3b3a36',
+    },
+  },
+}
