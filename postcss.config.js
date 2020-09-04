f81b72765dab701a9ba62c976404e1a400b40875
Stetson Ramey <stetson.ramey@gmail.com>
2020-09-04 14:12:59 -0700

    last edits to rehang 2020

--- postcss.config.js
+++ postcss.config.js
@@ -4,5 +4,6 @@ module.exports = {
     require('tailwindcss'),
     require('postcss-nested'),
     require('autoprefixer'),
+    ...(process.env.NODE_ENV === 'production' ? [require("cssnano")] : []),
   ],
 }
