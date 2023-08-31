b44fcd8c0b627c6b859237b4748faa90c478dfbd
Stetson Ramey <stetson.ramey@gmail.com>
2023-08-31 15:46:52 -0700

    try redirect from a function

---
+++ netlify/functions/prg.js
@@ -0,0 +1,11 @@
+// post / request / get pattern with a function
+export const handler = async (event) => {
+  // don't need to do anything but return to the
+  // redirect page so dupe form submissons don't happen
+  return {
+    statusCode: 302,
+    headers: {
+      Location: "https://tistheseasonkc.com/thank-you"
+    }
+  }
+}
